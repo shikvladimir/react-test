1de82ae461acd105fd2e636a5fe9053669177d23
Shik Vladimir <vnstore2018@gmail.com>
2021-11-23 11:47:12 +0300

    refactor|Todoitem|changing a parameter props

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -4,13 +4,13 @@ const TodoItem = (props) => {
   return (
     <div className="card" style={{ width: "18rem" }}>
       <img className="photo"
-        src={props.todos}
+        src={props.todos.img}
         alt="photo hastag" />
       <p className="card-text">
-        {props.text}
+        {props.todos.text}
       </p>
       <div className="form-check">
-        <input className="form-check-input" type="checkbox" value="">{props.isActiv}</input>
+        <input className="form-check-input" type="checkbox" value="{props.todos.isActiv}"/>
         <label>
           Запомнить
         </label>
